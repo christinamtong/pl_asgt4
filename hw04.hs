71e06bd1449c5e36b5f232193099edaa36fa461f
christinamtong <christinamtong@yahoo.com>
2016-10-10 15:00:00 -0700

    indenting and aexp working

--- hw04.hs
+++ hw04.hs
@@ -1,46 +1,48 @@
 -- Homework 4.0: Parsing with Applicative
 -- Due 2016-10-10
 
- {-# OPTIONS_GHC -Wall -fno-warn-unused-imports #-}
+{-# OPTIONS_GHC -Wall -fno-warn-unused-imports #-}
 
- module Hw04 where
+module Hw04 where
 
- import Control.Applicative
- import Data.Char
+import Control.Applicative
+import Data.Char
 
- import qualified Data.Map as Map
- import Data.Map (Map)
+import qualified Data.Map as Map
+import Data.Map (Map)
 
 -- In this homework, you'll write some pretty-printers and parsers for
 -- the While language of HW03.
 
- type VarName = String
+type VarName = String
 
- data AExp =
-     Var VarName
+data AExp =
+   Var VarName
    | Num Int
    | Plus AExp AExp
    | Times AExp AExp
    | Neg AExp
    | Div AExp AExp
-   deriving (Show, Eq)
+ deriving (Show, Eq)
 
- data BExp =
-     Bool Bool
-   | Equal AExp AExp
-   | Lt AExp AExp
-   | Not BExp
-   | Or BExp BExp
-   | And BExp BExp
-   deriving (Show, Eq)
+-- TODO FIX SPACING
 
- data Stmt =
-     Skip
-   | Assign VarName AExp
-   | Seq Stmt Stmt
-   | If BExp Stmt Stmt
-   | While BExp Stmt
-   deriving (Show, Eq)
+data BExp =
+   Bool Bool
+ | Equal AExp AExp
+ | Lt AExp AExp
+ | Not BExp
+ | Or BExp BExp
+ | And BExp BExp
+ deriving (Show, Eq)
+
+data Stmt =
+   Skip
+ | Assign VarName AExp
+ | Seq Stmt Stmt
+ | If BExp Stmt Stmt
+ | While BExp Stmt
+ deriving (Show, Eq)
 
 
 -- <h3>Problem 1: Pretty printing with `Show`</h3>
@@ -93,7 +95,7 @@
 -- (2) parses part of the input (at type `a`) and has some remaining,
 -- unparsed input.
 
- newtype Parser a = Parser { parse :: String -> Maybe (a,String) }
+newtype Parser a = Parser { parse :: String -> Maybe (a,String) }
 
 -- We'll start out by defining a few necessary instances, but our goal
 -- isn't to write `Parser (\s -> ...)` by hand: we'll construct a library
@@ -101,8 +103,8 @@
 
 -- But first, `Parser` is a functor: `fmap` applies to the parsed value.
 
- instance Functor Parser where
-   fmap f p = Parser $ \s -> (\(a,c) -> (f a, c)) <$> parse p s
+instance Functor Parser where
+ fmap f p = Parser $ \s -> (\(a,c) -> (f a, c)) <$> parse p s
 
 -- `Parser` is also applicative: a pure `Parser` always succeeds, just
 -- returning its input. The `(<*>)` (read: "ap") function takes an `f ::
@@ -112,12 +114,12 @@
 -- sure to apply `g` to the result. (Note how we use `fmap` to concisely
 -- say "apply `g` to the result of parsing with `a`.)
 
- instance Applicative Parser where
-   pure a = Parser $ \s -> Just (a,s)
-   f <*> a = Parser $ \s ->
-     case parse f s of
-       Just (g,s') -> parse (fmap g a) s'
-       Nothing -> Nothing
+instance Applicative Parser where
+ pure a = Parser $ \s -> Just (a,s)
+ f <*> a = Parser $ \s ->
+   case parse f s of
+     Just (g,s') -> parse (fmap g a) s'
+     Nothing -> Nothing
 
 -- There's one last type class to define: `Alternative`, which is a
 -- left-biased choice. In the `empty` case, we just return
@@ -125,9 +127,9 @@
 -- the existing `Alternative` class for `Maybe`: try to parse the
 -- left-hand side, and if that fails, try the right.
 
- instance Alternative Parser where
-   empty = Parser $ \s -> Nothing
-   l <|> r = Parser $ \s -> parse l s <|> parse r s
+instance Alternative Parser where
+ empty = Parser $ \s -> Nothing
+ l <|> r = Parser $ \s -> parse l s <|> parse r s
 
 -- Below we'll define a few "primitive" parsers, which explicitly write
 -- parsing functions. Don't define any extra ones! All of your solutions
@@ -135,26 +137,26 @@
 -- Functor/Applicative/Alternative, and (c) other helpers/recursive
 -- functions you define.
 
- ensure :: (a -> Bool) -> Parser a -> Parser a
- ensure p parser = Parser $ \s ->
-    case parse parser s of
-      Nothing -> Nothing
-      Just (a,s') -> if p a then Just (a,s') else Nothing
+ensure :: (a -> Bool) -> Parser a -> Parser a
+ensure p parser = Parser $ \s ->
+  case parse parser s of
+    Nothing -> Nothing
+    Just (a,s') -> if p a then Just (a,s') else Nothing
 
- lookahead :: Parser (Maybe Char)
- lookahead = Parser f
-   where f [] = Just (Nothing,[])
-         f (c:s) = Just (Just c,c:s)
+lookahead :: Parser (Maybe Char)
+lookahead = Parser f
+ where f [] = Just (Nothing,[])
+       f (c:s) = Just (Just c,c:s)
 
- satisfy :: (Char -> Bool) -> Parser Char
- satisfy p = Parser f
-   where f [] = Nothing
-         f (x:xs) = if p x then Just (x,xs) else Nothing
+satisfy :: (Char -> Bool) -> Parser Char
+satisfy p = Parser f
+ where f [] = Nothing
+       f (x:xs) = if p x then Just (x,xs) else Nothing
 
 -- We can detect the end of the file manually...
 
- eof :: Parser ()
- eof = Parser $ \s -> if null s then Just ((),[]) else Nothing
+eof :: Parser ()
+eof = Parser $ \s -> if null s then Just ((),[]) else Nothing
 
 -- ...but the other three primitive parsers are enough on their
 -- own.
@@ -162,39 +164,39 @@
 -- **HWHWHW** Write a function `eof'` which behaves like `eof` but is built up
 -- from the other primitive parsers:
 
- eof' :: Parser ()
- eof' = (\s -> (())) <$> (ensure (\s -> s==Nothing) lookahead)
+eof' :: Parser ()
+eof' = (\s -> (())) <$> (ensure (\s -> s==Nothing) lookahead)
 
 -- In general, we'll be careful to allow arbitrary whitespace at the
 -- beginnings of what we parse.
 
- ws :: Parser ()
- ws = pure () <* many (satisfy isSpace)
+ws :: Parser ()
+ws = pure () <* many (satisfy isSpace)
 
- char :: Char -> Parser Char
- char c = ws *> satisfy (==c)
+char :: Char -> Parser Char
+char c = ws *> satisfy (==c)
 
- str :: String -> Parser String
- str s = ws *> loop s
-   where loop [] = pure []
-         loop (c:cs) = (:) <$> satisfy (==c) <*> loop cs
+str :: String -> Parser String
+str s = ws *> loop s
+ where loop [] = pure []
+       loop (c:cs) = (:) <$> satisfy (==c) <*> loop cs
 
 -- We can define what it means to parse something in parentheses. Note
 -- how we're careful to specify order of operations between `(*>)` and
 -- `(<*)`---forgetting to do this can lead to some tricky bugs!
 
- parens :: Parser a -> Parser a
- parens p = (char '(' *> p) <* char ')'
+parens :: Parser a -> Parser a
+parens p = (char '(' *> p) <* char ')'
 
 -- Let's start our parser from the bottom up. We'll begin with a notion
 -- of *keyword* a/k/a a *reserved word*. These are English words that
 -- can't be used as variables because they'll be part of the concrete
 -- syntax of our language.
 
- keywords :: [String]
- keywords = ["skip","if","then","else","while","true","false"]
+keywords :: [String]
+keywords = ["skip","if","then","else","while","true","false"]
 
- isKeyword = (`elem` keywords)
+isKeyword = (`elem` keywords)
 
 -- To parse a keyword, we'll parse it as a string (using `str`) but make
 -- sure that it it's a whole strong. For example, `parse (kw "repeat")
@@ -205,18 +207,18 @@
 -- that the given string is in the list `keywords`).
 
 
- kw :: String -> Parser String
- kw s = ws *> ((str s) <* (ensure isNotAlphaNumeric lookahead))
-     where isNotAlphaNumeric Nothing = True
-           isNotAlphaNumeric (Just (c)) = not (isAlphaNum c)
+kw :: String -> Parser String
+kw s = ws *> ((str s) <* (ensure isNotAlphaNumeric lookahead))
+   where isNotAlphaNumeric Nothing = True
+         isNotAlphaNumeric (Just (c)) = not (isAlphaNum c)
 
 
 -- **HWHWHW** Now let's parse variables. A variable is (1) a string
 -- starting with an alphabetical character followed by zero or more
 -- alphanumeric characters that (2) isn't a keyword.
 
- var :: Parser String
- var = ws *> (ensure (\s -> not (isKeyword s)) ((:) <$> satisfy isAlpha <*> many (satisfy isAlphaNum)))
+var :: Parser String
+var = ws *> (ensure (\s -> not (isKeyword s)) ((:) <$> satisfy isAlpha <*> many (satisfy isAlphaNum)))
 
 
 -- A number is one or more digits. We can use `read :: Read a => String
@@ -224,66 +226,28 @@
 -- the Prelude. Note that the type annotation here is critical, or
 -- Haskell will complain---it doesn't know what you want to read!
 
- num :: Parser Int
- num = ws *> (read <$> some (satisfy isDigit))
+num :: Parser Int
+num = ws *> (read <$> some (satisfy isDigit))
 
--- -- > ensure :: (a -> Bool) -> Parser a -> Parser a
--- -- > ensure p parser = Parser $ \s ->
--- -- >    case parse parser s of
--- -- >      Nothing -> Nothing
--- -- >      Just (a,s') -> if p a then Just (a,s') else Nothing
--- -- >
--- -- > lookahead :: Parser (Maybe Char)
--- -- > lookahead = Parser f
--- -- >   where f [] = Just (Nothing,[])
--- -- >         f (c:s) = Just (Just c,c:s)
--- --
--- -- > satisfy :: (Char -> Bool) -> Parser Char
--- -- > satisfy p = Parser f
--- -- >   where f [] = Nothing
--- -- >         f (x:xs) = if p x then Just (x,xs) else Nothing
-
--- instance Show AExp where
---    show (Var a) = a
---    show (Num a) = show a
---    show (Plus a b) = showTerm (Plus a b)
---    show (Div a b) = showFactor (Div a b)
---    show (Times a b) = showFactor (Times a b)
---    show (Neg a) = showNeg (Neg a)
-
---  showTerm (Plus a (Neg b)) = showFactor a ++ " - " ++ showTerm b
---  showTerm (Plus a b) = showFactor a ++ " + " ++ showTerm b
---  showTerm x = showFactor x
-
---  showFactor (Times a b) = showNeg a ++ " * " ++ showFactor b
---  showFactor (Div a b) = showNeg a ++ " / " ++ showFactor b
---  showFactor a = showNeg a
-
---  showNeg (Neg a) = "-" ++ showAtom a
---  showNeg a = showAtom a
-
---  showAtom (Num a) = show a
---  showAtom (Var a) = a
---  showAtom a = "(" ++ showTerm a ++ ")"
 
 -- **HWHWHW** Now define an `AExp` parser; call the top-level "terms"
 -- `aexp`, but you can call the rest whatever you want. Feel free to
 -- copy/paste most of it from lecture notes, but don't forget to add
 -- division! Please also encode subtraction using addition and negation.
 
- aexp, factor, neg, atom :: Parser AExp
- aexp = Plus <$> factor <* plus <*> aexp
-        <|> (\a b -> Plus a (Neg b)) <$> factor <* minus <*> aexp
-        <|> factor
- factor = Times <$> neg <* times <*> factor <|> neg
- neg = Neg <$> (minus *> atom) <|> atom
- atom = Num <$> num <|> (char '(' *> aexp <* char ')') <|> Var <$> var
+aexp, factor, neg, atom :: Parser AExp
+aexp = Plus <$> factor <* plus <*> aexp
+      <|> (\a b -> Plus a (Neg b)) <$> factor <* minus <*> aexp
+      <|> factor
+factor = Times <$> neg <* times <*> factor <|> neg
+neg = Neg <$> (minus *> atom) <|> atom
+atom = Num <$> num <|> (char '(' *> aexp <* char ')') <|> Var <$> var
 
- plus, divide, minus, times :: Parser Char
- plus = char '+' 
- divide = char '/'
- minus = char '-'
- times = char '*'
+plus, divide, minus, times :: Parser Char
+plus = char '+' 
+divide = char '/'
+minus = char '-'
+times = char '*'
 
 
 -- **HWHWHW** Now let's define a `BExp` parser; we'll have to use the
@@ -298,30 +262,31 @@
 -- is `&&`, or is `||`, and not is `!`. The constants can just use
 -- lower-case names, like `true` and `false`.
 
- bexp_encoding_test1 = "x == y" -- Equal (Var x) (Var y)
- bexp_encoding_test2 = "x <= y" -- Or (Equal (Var x) (Var y)) (Lt (Var x) (Var y))
- bexp_encoding_test3 = "x != y" -- Not (Equal (Var x)) (Var y)
+bexp_encoding_test1 = "x == y" -- Equal (Var x) (Var y)
+bexp_encoding_test2 = "x <= y" -- Or (Equal (Var x) (Var y)) (Lt (Var x) (Var y))
+bexp_encoding_test3 = "x != y" -- Not (Equal (Var x)) (Var y)
 
- bexp, factorB, notB, atomB :: Parser BExp
- bexp = Or <$> factorB <* or' <*> bexp <|> factorB
- factorB = And <$> notB <* and' <*> factorB <|> notB
- notB = Not <$> (bang' *> atomB) <|> atomB
- atomB = AExp <$> aexp <|> (char '(' *> bexp <* char ')')
-        <|> Equal <$> factorB <* equal' <*> bexp
-        <|> Not <$> Equal <$> factorB <* nequal' <*> bexp
-        <|> Lt <$> factorB <* lt' <*> bexp
-		 <|> Or <$> (Equal <$> factorB <* leq' <*> bexp) <$> (Lt <$> factorB <* lt' <*> bexp)
-        <|> Not <$> Lt <$> factorB <* geq' <*> bexp
+bexp, factorB, notB, atomB :: Parser BExp
+bexp = Or <$> factorB <* or' <*> bexp <|> factorB
+factorB = And <$> notB <* and' <*> factorB <|> notB
+notB = Not <$> (bang' *> atomB) <|> atomB
+atomB = AExp <$> aexp <|> (char '(' *> bexp <* char ')')
+      <|> Equal <$> factorB <* equal' <*> bexp
+      <|> Not <$> Equal <$> factorB <* nequal' <*> bexp
+      <|> Lt <$> factorB <* lt' <*> bexp
+	    <|> Or <$> (Equal <$> factorB <* leq' <*> bexp) <$> (Lt <$> factorB <* lt' <*> bexp)
+      <|> Not <$> Lt <$> factorB <* geq' <*> bexp
 
- or' = char '||'
- and' = char '&&'
- bang' = char '!'
- equal' = char '=='
- nequal' = char '!='
- lt' = char '<'
- gt' = char '<'
- leq' = char '<='
- geq' = char '>='
+or', and', bang', equal', nequal', lt', gt', leq', geq' :: Parser Char
+or' = str "||"
+and' = str "&&"
+bang' = char "!"
+equal' = str "=="
+nequal' = str "!="
+lt' = char "<"
+gt' = char ">"
+leq' = str "<="
+geq' = str ">="
 
 -- <h3>Problem 3: Parsing a la C</h3>
 
@@ -335,18 +300,18 @@
 
 -- Here are some example programs in the C syntax:
 
- cProg1 = "x = 5; y = 6;"
- cProg2 = "if (x == 0) { y = 10; } else { y = x; }"
- cProg3 = "while (iffy > 10) { iffy = iffy - 1; };\n\nderp = 7;"
- cProg4 = "while (true) {};"
+cProg1 = "x = 5; y = 6;"
+cProg2 = "if (x == 0) { y = 10; } else { y = x; }"
+cProg3 = "while (iffy > 10) { iffy = iffy - 1; };\n\nderp = 7;"
+cProg4 = "while (true) {};"
 
 -- You'll *definitely* want to write more test programs. If you come up
 -- with interesting corner cases, post them to Piazza! I want to
 -- explicitly encourage collaboration on understanding the syntax (though
 -- please write your own parsers within your pair).
 
- cSyntax :: Parser Stmt
- cSyntax = undefined
+cSyntax :: Parser Stmt
+cSyntax = undefined
 
 
 -- Note: you will *not* be penalized for extra `Skip`s in your parsed
@@ -369,29 +334,29 @@
 
 -- Here's a sample program:
 
- pyProg1 = "x = 5\ny = 6"
+pyProg1 = "x = 5\ny = 6"
 
 -- Oy---those newlines are going to get tiresome. There's a handy
 -- function, `unlines`, which will let us write this a clearer way:
 
- pyProg1' = unlines ["x = 5",
-                     "y = 6"]
+pyProg1' = unlines ["x = 5",
+                   "y = 6"]
 
- pyProg2 = unlines ["if x == 0:",
-                    "  y = 10",
-                    "else:",
-                    "  y = x"]
- pyProg3 = unlines ["while iffy > 10: ",
-                    "  iffy = iffy - 1"]
- pyProg4 = unlines ["while false:",
-                    "  ",
-                    "x = 0"]
- pyProg5 = unlines ["while x > 0:",
-                    "  while y < x:",
-                    "    x = x - 1",
-                    "    y = y + 1",
-                    "  x = x - 1",
-                    ""]
+pyProg2 = unlines ["if x == 0:",
+                  "  y = 10",
+                  "else:",
+                  "  y = x"]
+pyProg3 = unlines ["while iffy > 10: ",
+                  "  iffy = iffy - 1"]
+pyProg4 = unlines ["while false:",
+                  "  ",
+                  "x = 0"]
+pyProg5 = unlines ["while x > 0:",
+                  "  while y < x:",
+                  "    x = x - 1",
+                  "    y = y + 1",
+                  "  x = x - 1",
+                  ""]
 
 -- I've tried to cover the corner cases with these examples, but please
 -- do discuss them. I'll try to settle any ambiguities that may arise.
@@ -400,8 +365,8 @@
 -- Python. Your top-level parser of statements should be named
 -- `pythonSyntax`:
 
- pythonSyntax :: Parser Stmt
- pythonSyntax = undefined
+pythonSyntax :: Parser Stmt
+pythonSyntax = undefined
 
 -- You can largely follow the same idea as your C-style parser, but your
 -- parsers for individual statements will need to keep track of the
